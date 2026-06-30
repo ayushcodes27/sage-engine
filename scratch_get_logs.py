0334624925f279e856dacff19f0852b62bf1c478
Ayush <deoreayush555@gmail.com>
2026-06-30 21:46:05 +0530

    Cleanup task

--- scratch_get_logs.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-
-with open("ml_logs.txt", "w") as f:
-    result = subprocess.run(["docker", "logs", "sage-ml"], capture_output=True, text=True)
-    f.write("STDOUT:\n")
-    f.write(result.stdout)
-    f.write("\nSTDERR:\n")
-    f.write(result.stderr)
